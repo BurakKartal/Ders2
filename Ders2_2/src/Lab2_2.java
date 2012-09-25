bba38e612ef18727d3cbb2e5002d4b42d227dde8
20090105016 <20090105016@muhlab12.lab.khas.edu.tr>
2012-09-25 08:09:50 -0430

    Commit message with multiplication

--- Ders2_2/src/Lab2_2.java
+++ Ders2_2/src/Lab2_2.java
@@ -4,11 +4,15 @@ public class Lab2_2 {
 	public static int addition(int a, int b){
 		return a+b;
 	}
+	public static int multiplication(int a, int b){
+		return a*b;
+	}
 
 	public static void main(String[] args) {
 		int a = 5;
 		int b = 6;
 		System.out.println(addition(a, b));
+		System.out.println(multiplication(a, b));
 	}
 
 }
