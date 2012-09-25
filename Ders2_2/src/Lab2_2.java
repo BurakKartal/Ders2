ebe14be1ac650af95343afc6853b6d76421ff2fe
20090105016 <20090105016@muhlab12.lab.khas.edu.tr>
2012-09-25 08:18:22 -0430

    Fibonacci Series

--- Ders2_2/src/Lab2_2.java
+++ Ders2_2/src/Lab2_2.java
@@ -1,18 +1,25 @@
+import javax.swing.JOptionPane;
 
 public class Lab2_2 {
 	
-	public static int addition(int a, int b){
-		return a+b;
-	}
-	public static int multiplication(int a, int b){
-		return a*b;
-	}
-
-	public static void main(String[] args) {
-		int a = 5;
-		int b = 6;
-		System.out.println(addition(a, b));
-		System.out.println(multiplication(a, b));
-	}
-
-}
+	public static void main(String[] args)
+	    {  
+	       String input = JOptionPane.showInputDialog("Enter n: ");
+	       int n = Integer.parseInt(input);
+	 
+	       for (int i = 1; i <= n; i++)
+	       {
+	          int f = fib(i);
+	          System.out.println("fib(" + i + ") = " + f);
+	       }
+	       System.exit(0);
+	       
+	    }
+	    public static int fib(int n)
+	    {  
+	       if (n <= 2)
+	            return 1;
+	       else 
+	            return fib(n - 1) + fib(n - 2);
+	    }
+	 }
